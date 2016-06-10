592bf6494a48084ed1ef46c38782af24e4477669
Augusto Nishi <Augusto Nishi>
2016-06-10 19:49:05 -0300

    Add references to the project

--- SAP/SAP/MainWindow.xaml.cs
+++ SAP/SAP/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Emgu.CV;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
