55b1e272bd7e9e10163201c5fb88e307fa4b3226
Augusto Nishi <Augusto Nishi>
2016-06-10 21:03:52 -0300

    Add picture model and add Surf detection

--- SAP/SAP/MainWindow.xaml.cs
+++ SAP/SAP/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Emgu.CV;
+using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,29 +27,56 @@ namespace SAP
     {
         private Capture capture;
         private Timer timer;
+        private Image<Bgr, byte> model;
+
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        /// <summary>
+        /// Convert an IImage to a WPF BitmapSource. The result can be used in the Set Property of Image.Source
+        /// </summary>
+        /// <param name="image">The Emgu CV Image</param>
+        /// <returns>The equivalent BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using (System.Drawing.Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap
+
+                BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap
+                return bs;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-            timer = new Timer(GetPic, null, 0, 33);
+            model = new Image<Bgr, byte>("model.png");
+            capture = new Capture("penalty.mp4");
+            timer = new Timer(GetPic, null, 0, 1000);
         }
 
         private void GetPic(object state)
         {
-            capture = new Capture();
             var queryframe = capture.QueryFrame();
             if (queryframe != null)
             {
-                using (MemoryStream memory = new MemoryStream())
+                long time;
+                var detected = SurfUtil.Draw(queryframe, model.Mat, out time);
+                this.Dispatcher.Invoke((Action)(() =>
                 {
-                    queryframe.Bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-                    memory.Position = 0;
-                    BitmapImage bitmapimage = new BitmapImage();
-                    bitmapimage.BeginInit();
-                    bitmapimage.StreamSource = memory;
-                    bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-                    bitmapimage.EndInit();
-                    display.Source = bitmapimage;
-                }
+                     display.Source = ToBitmapSource(detected);
+                }));
+            }
+            else
+            {
+                timer.Dispose();
             }
         }
     }
